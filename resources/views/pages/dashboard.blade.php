129b13b46aeb95307ba3a00fd730f3b5e6e0f9e1
chandra <candrasachio@gmail.com>
2023-08-21 20:20:07 +0700

    add title dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
-
+@section('title')
+Dashboard
+@endsection
 @section('content')
 <div class="container-xxl flex-grow-1 container-p-y">
     <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Siswa /</span> Profile
