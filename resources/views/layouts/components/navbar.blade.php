ba7004fc16cca484678c7fc3c3a2a69655f47c1d
chandra <candrasachio@gmail.com>
2023-08-29 00:27:03 +0700

    fix error on setting account

--- resources/views/layouts/components/navbar.blade.php
+++ resources/views/layouts/components/navbar.blade.php
@@ -22,7 +22,7 @@
                     <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                         <div class="avatar avatar-online">
                             @if(Auth::user()->role_name == 'Student')
-                            @if($student_document)
+                            @if(!isset($student_document))
                             <img src="{{ $student_document ? asset('storage/'.$student_document->pas_photo) : '../../assets/img/avatars/1.png' }}" alt class="h-auto rounded-circle" />
                             @else
                             <img src="../../assets/img/avatars/1.png" alt class="h-auto rounded-circle" />
