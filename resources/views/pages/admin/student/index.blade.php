300c8d7908cfd60cca6acc4b4f6d896d8a9a1554
chandra <candrasachio@gmail.com>
2023-08-29 00:50:34 +0700

    download file

--- resources/views/pages/admin/student/index.blade.php
+++ resources/views/pages/admin/student/index.blade.php
@@ -11,11 +11,11 @@ Detail Siswa
         <div class="col-md-12">
             <ul class="nav nav-pills flex-column flex-sm-row mb-4">
                     <a class="btn btn-primary btn-sm" style="margin-right: 10px;" href="{{ asset('storage/'.$student_document->pas_photo) }}" download><i class="ti-xs ti ti-file me-1"></i> Foto Siswa</a>
-                    <a class="btn btn-primary btn-sm" style="margin-right: 10px;" href="{{ asset('storage/'.$student_document->sd_certificate) }}"><i class="ti-xs ti ti-file me-1"></i> Ijazah SD</a>
-                    <a class="btn btn-primary btn-sm" style="margin-right: 10px;" href="{{ asset('storage/'.$student_document->smp_certificate) }}"><i class="ti-xs ti ti-file me-1"></i> Ijazah SMP</a>
-                    <a class="btn btn-primary btn-sm" style="margin-right: 10px;" href="{{ asset('storage/'.$student_document->birth_certificate) }}"><i class="ti-xs ti ti-file me-1"></i> Akta Kelahiran</a>
-                    <a class="btn btn-primary btn-sm" style="margin-right: 10px;" href="{{ asset('storage/'.$student_document->family_card) }}"><i class="ti-xs ti ti-file me-1"></i> Kartu Keluarga</a>
-                    <a class="btn btn-primary btn-sm" style="margin-right: 10px;" href="{{ asset('storage/'.$student_document->signature) }}"><i class="ti-xs ti ti-file me-1"></i> Tanda Tangan</a>
+                    <a class="btn btn-primary btn-sm" style="margin-right: 10px;" href="{{ asset('storage/'.$student_document->sd_certificate) }}" download><i class="ti-xs ti ti-file me-1"></i> Ijazah SD</a>
+                    <a class="btn btn-primary btn-sm" style="margin-right: 10px;" href="{{ asset('storage/'.$student_document->smp_certificate) }}" download><i class="ti-xs ti ti-file me-1"></i> Ijazah SMP</a>
+                    <a class="btn btn-primary btn-sm" style="margin-right: 10px;" href="{{ asset('storage/'.$student_document->birth_certificate) }}" download><i class="ti-xs ti ti-file me-1"></i> Akta Kelahiran</a>
+                    <a class="btn btn-primary btn-sm" style="margin-right: 10px;" href="{{ asset('storage/'.$student_document->family_card) }}" download><i class="ti-xs ti ti-file me-1"></i> Kartu Keluarga</a>
+                    <a class="btn btn-primary btn-sm" style="margin-right: 10px;" href="{{ asset('storage/'.$student_document->signature) }}" download><i class="ti-xs ti ti-file me-1"></i> Tanda Tangan</a>
             </ul>
         </div>
     </div>
