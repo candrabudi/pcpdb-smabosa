f627ed5e7f0925e7232c69db54494d690c702dbf
candrabudi <candrasachio@gmail.com>
2023-09-25 07:13:48 +0700

    fix auth admin

---
+++ resources/views/admin/auth/utils/steper_header.blade.php
@@ -0,0 +1,35 @@
+<div class="bs-stepper-header border-bottom-0">
+    <div class="step" data-target="#accountDetailsValidation">
+        <button type="button" class="step-trigger">
+            <span class="bs-stepper-circle"><i class="ti ti-smart-home ti-sm"></i></span>
+            <span class="bs-stepper-label">
+                <span class="bs-stepper-title">Akun</span>
+                <span class="bs-stepper-subtitle">Detail Akun</span>
+            </span>
+        </button>
+    </div>
+    <div class="line">
+        <i class="ti ti-chevron-right"></i>
+    </div>
+    <div class="step" data-target="#personalInfoValidation">
+        <button type="button" class="step-trigger">
+            <span class="bs-stepper-circle"><i class="ti ti-users ti-sm"></i></span>
+            <span class="bs-stepper-label">
+                <span class="bs-stepper-title">Personal</span>
+                <span class="bs-stepper-subtitle">Lengkapi Data Diri Pribadi</span>
+            </span>
+        </button>
+    </div>
+    <div class="line">
+        <i class="ti ti-chevron-right"></i>
+    </div>
+    <div class="step" data-target="#schoolInfoValidation">
+        <button type="button" class="step-trigger">
+            <span class="bs-stepper-circle"><i class="ti ti-file-text ti-sm"></i></span>
+            <span class="bs-stepper-label">
+                <span class="bs-stepper-title">Asal Sekolah</span>
+                <span class="bs-stepper-subtitle">Masukan Detail Asal Sekolah</span>
+            </span>
+        </button>
+    </div>
+</div>
